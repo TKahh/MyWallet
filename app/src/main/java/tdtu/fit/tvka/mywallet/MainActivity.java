35d3904ce05872772b96c22e236d545095bd9e97
Ky Anh <trankyanh202@gmail.com>
2023-09-28 21:09:02 +0700

    hello text

---
+++ app/src/main/java/tdtu/fit/tvka/mywallet/MainActivity.java
@@ -0,0 +1,15 @@
+package tdtu.fit.tvka.mywallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hello there
+    }
+}
